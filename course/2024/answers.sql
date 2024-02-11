242f7601835876d7042ef327873eaccb8c794bd4
Shivani Baskar <58472391+shivanibaskar@users.noreply.github.com>
2024-02-11 17:30:36 -0500

    Update answers.sql

--- course/2024/answers.sql
+++ course/2024/answers.sql
@@ -1,4 +1,20 @@
-  -- Question 1. What is count of records for the 2022 Green Taxi Data?
+-- creating an external table
+CREATE OR REPLACE EXTERNAL TABLE
+  `absolute-axis-409317.green_taxi.2022_data` OPTIONS ( format ="PARQUET",
+    uris = ['gs://absolute-axis-409317-terra-bucket/2022_greentrip/green_tripdata_2022*'] );
+
+
+--   Creating a biq query table from the previous table
+CREATE TABLE
+  green_taxi.trips_2022 AS (
+  SELECT
+    *
+  FROM
+    green_taxi.2022_data );
+
+
+
+-- Question 1. What is count of records for the 2022 Green Taxi Data?
 SELECT
   COUNT(*)
 FROM
